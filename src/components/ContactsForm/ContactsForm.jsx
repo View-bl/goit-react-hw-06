6c753ccb88816c613b30e3978bf3953433e2ae98
Roman Fedorchuk <Romaitgo@gmail.com>
2025-02-24 12:37:32 +0100

    f

--- src/components/ContactsForm/ContactsForm.jsx
+++ src/components/ContactsForm/ContactsForm.jsx
@@ -2,21 +2,31 @@ import { useState } from "react";
 import { TextField, Button, Box } from "@mui/material";
 import { useDispatch } from "react-redux";
 import { addContact } from "../../redux/contactsSlice";
+import { v4 as uuidv4 } from "uuid"; 
 
-const ContactsForm = () => {
+const ContactForm = () => {
   const [name, setName] = useState("");
   const [phone, setPhone] = useState("");
   const dispatch = useDispatch();
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    dispatch(addContact({ name, phone }));
+    const newContact = {
+      id: uuidv4(),
+      name,
+      phone,
+    };
+    dispatch(addContact(newContact)); 
     setName("");
     setPhone("");
   };
 
   return (
-    <Box component="form" onSubmit={handleSubmit} sx={{ display: 'flex', flexDirection: 'column', gap: 2 }}>
+    <Box
+      component="form"
+      onSubmit={handleSubmit}
+      sx={{ display: "flex", flexDirection: "column", gap: 2 }}
+    >
       <TextField
         label="Ім'я"
         value={name}
@@ -36,4 +46,4 @@ const ContactsForm = () => {
   );
 };
 
-export default ContactsForm;
+export default ContactForm;
