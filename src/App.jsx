7fa0b1c95ce55281448c9b4afa99c4617729a9e3
Roman Fedorchuk <Romaitgo@gmail.com>
2025-02-24 08:50:35 +0100

    p

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,22 @@
-import { Provider } from "react-redux";
-import { PersistGate } from "redux-persist/integration/react";
-import { store, persistor } from './redux/store';
-import { Container, Typography, TextField, Button, List, ListItem, ListItemText } from '@mui/material';
-import ContactsForm from './components/ContactsForm/ContactsForm';
-import SearchBox from './components/SearchBox/SearchBox';
-import ContactList from './components/ContactList/ContactList';
+import { Container, Typography, List } from "@mui/material";
+import ContactsForm from "./components/ContactsForm/ContactsForm";
+import SearchBox from "./components/SearchBox/SearchBox";
+import ContactList from "./components/ContactList/ContactList";
 
 const App = () => {
   return (
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <Container maxWidth="sm" sx={{ paddingTop: 4 }}>
-          <Typography variant="h3" component="h1" gutterBottom align="center">
-            Книга контактів
-          </Typography>
+    <Container maxWidth="sm" sx={{ paddingTop: 4 }}>
+      <Typography variant="h3" component="h1" gutterBottom align="center">
+        Книга контактів
+      </Typography>
 
-          <ContactsForm />
+      <ContactsForm />
+      <SearchBox />
 
-          <SearchBox />
-
-          <List>
-            <ContactList />
-          </List>
-        </Container>
-      </PersistGate>
-    </Provider>
+      <List>
+        <ContactList />
+      </List>
+    </Container>
   );
 };
 
